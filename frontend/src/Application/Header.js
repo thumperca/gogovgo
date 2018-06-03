3aeef5353d5a3ed77c515b48822aad15b76cf06f
Manvir <mail@manvir.me>
2018-06-03 18:52:46 +0530

    header changes when user login

--- frontend/src/Application/Header.js
+++ frontend/src/Application/Header.js
@@ -5,12 +5,12 @@
 import React, { Component } from "react";
 import HowItWorks from "./HowItWorks";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
-import { withRouter } from "react-router-dom";
+import { withRouter, Link } from "react-router-dom";
 
 class Header extends Component {
     constructor(props, context) {
         super(props, context);
-        this.state = { howItWorksShow: false };
+        this.state = { howItWorksShow: false, login: true, dropdown: true };
     }
 
     signup() {
@@ -24,6 +24,33 @@ class Header extends Component {
             marginRight: "5px",
             fontSize: "17px"
         };
+
+        let navItem = (
+            <Nav pullRight>
+                <NavItem className="nav-link how-it-work" onClick={() => this.signup()}>
+                    <i className="fa fa-user-circle" style={userIcon} />
+                    Sign up
+                </NavItem>
+            </Nav>
+        );
+        if (this.state.login) {
+            navItem = (
+                <Nav pullRight>
+                    <NavItem
+                        className="nav-link how-it-work user"
+                        onClick={() => this.setState({ dropdown: !this.state.dropdown })}
+                    >
+                        <span>Manvir</span>
+                        <i className="fa fa-angle-down" />
+                        <i className="fa fa-user-circle" />
+                        <div className="profile-option-mobile">
+                            <a>Edit Profile</a>
+                            <a>Logout</a>
+                        </div>
+                    </NavItem>
+                </Nav>
+            );
+        }
 
         return (
             <Navbar className="main-menu" collapseOnSelect fluid>
@@ -39,12 +66,13 @@ class Header extends Component {
                     <Navbar.Toggle />
                 </Navbar.Header>
                 <Navbar.Collapse>
-                    <Nav pullRight>
-                        <NavItem className="nav-link how-it-work" onClick={() => this.signup()}>
-                            <i className="fa fa-user-circle" style={userIcon} />
-                            Sign up
-                        </NavItem>
-                    </Nav>
+                    {this.state.dropdown && (
+                        <div className="profile-option">
+                            <a>Edit Profile</a>
+                            <a>Logout</a>
+                        </div>
+                    )}
+                    {navItem}
                     <Nav pullRight>
                         <NavItem
                             className="nav-link how-it-work"
